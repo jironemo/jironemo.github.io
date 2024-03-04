e7af09149aaee7da19d0dcbce1e0475a6d42f606
Aung Khaing Khant <aungkhaingkhant@securelinkmm.com>
2024-03-04 17:54:34 +0630

    Testing bearer authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,16 +22,33 @@ export class AppComponent {
     }
 };
   display:any ="";
- ngOnInit(){
-  const headers = new HttpHeaders({'Content-Type': 'application/json','apiKey': 'NJDSNSIOlsoihRBfsZNBHHgdJWIJsO2wtYzXEEDeo6j1JZS3B452CbzOXFXpozdh'});
 
-  var url:string = ("https://data.mongodb-api.com/app/data-dxrnu/endpoint/data/v1/find");
-    this.http.post(url, JSON.stringify(this.data), {headers})
-      .subscribe((document:any) => {
-        for (var i = 0; i < document.documents.length; i++) {
-          this.allPosts.push(document.documents[i].text);
-        }
-      });
+
+
+  generateAccessToken(): Observable<any> {
+    // Set up headers with apiKey
+    var tokenUrl = "https://services.cloud.mongodb.com/api/client/v2.0/app/data-dxrnu/auth/providers/api-key/login"
+    var apiKey = 'NJDSNSIOlsoihRBfsZNBHHgdJWIJsO2wtYzXEEDeo6j1JZS3B452CbzOXFXpozdh'
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    // Make POST request to tokenUrl
+    return this.http.post<any>(tokenUrl, {key:apiKey}, { headers });
+  }
+
+ ngOnInit(){
+  var accessToken:string = "";
+  this.generateAccessToken().subscribe((data:any) => {
+    const headers = new HttpHeaders({'Content-Type': 'application/json','Authorization': 'Bearer '+data.access_token });
+    var url:string = ("https://data.mongodb-api.com/app/data-dxrnu/endpoint/data/v1/action/find");
+      this.http.post(url, JSON.stringify(this.data), {headers})
+        .subscribe((document:any) => {
+          for (var i = 0; i < document.documents.length; i++) {
+            this.allPosts.push(document.documents[i].text);
+          }
+        });
+  });
+  
 }
   
 
